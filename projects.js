0b1529b38cef35b940057ab82f3d311aa1c002c2
Taira Kinjo <kinjo.taira0098@gmail.com>
2025-11-11 20:42:49 +0900

    Minor tweaks to website content and branding.

--- projects.js
+++ projects.js
@@ -3,35 +3,41 @@ window.PROJECTS = [
     id: "hall-table",
     title: "Hall Table",
     year: 2025,
-    size: "size",
-    wood: "wood type",
+    size: "Size",
+    wood: "Wood Type",
     price: "Price Negotiable",
     images: [
       "assets/book_table_gray.jpg",
       "assets/book_table.jpg"
     ],
-    description: "Table"
+    description: "Table",
+    buildNotes: ["Build notes go here"
+
+    ]
   },
   {
     id: "bench",
     title: "Benches and Chairs",
     year: 2025,
     size: "Size",
-    wood: "wood type",
+    wood: "Wood Type",
     price: "Price Negotiable",
     images: [
       "assets/Bench.jpg",
       "assets/chair_white.jpg",
       "assets/2_people_bench.jpg"
     ],
-    description: "Benches and Chairs"
+    description: "Benches and Chairs",
+    buildNotes: ["Build notes go here"
+
+    ]
   },
   {
     id: "misc-project",
     title: "Misc Project",
     year: 2025,
     size: "Size",
-    wood: "wood type",
+    wood: "Wood Type",
     price: "Price Negotiable",
     images: [
       "assets/cross.jpg",
@@ -40,6 +46,9 @@ window.PROJECTS = [
       "assets/wagon.jpg",
       "assets/utensil_holder.jpg"
     ],
-    description: "Miscellaneous Projects"
+    description: "Miscellaneous Projects",
+    buildNotes: ["Build notes go here"
+
+    ]
   }
 ];
